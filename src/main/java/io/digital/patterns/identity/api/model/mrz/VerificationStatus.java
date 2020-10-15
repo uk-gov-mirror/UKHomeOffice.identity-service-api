b78d8269fe94fc393d2572e279e1f66f618231a3
Mark Olliver <mark@digitalpatterns.io>
2020-10-15 13:32:31 +0100

    add hashes

--- src/main/java/io/digital/patterns/identity/api/model/mrz/VerificationStatus.java
+++ src/main/java/io/digital/patterns/identity/api/model/mrz/VerificationStatus.java
@@ -1,8 +1,14 @@
 package io.digital.patterns.identity.api.model.mrz;
 
+import io.digital.patterns.identity.api.model.airline.JourneyLeg;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class VerificationStatus {
@@ -16,6 +22,11 @@ public class VerificationStatus {
     private SigningCertificate documentSigning;
     private SigningCertificate countrySigning;
 
+    @Schema(description = "Chip verification hash", required = true)
+    @Min(1)
+    @Max(2)
+    private List<Hash> hashTable = new ArrayList<>();
+
 
     @Data
     public static class SigningCertificate {
@@ -23,10 +34,16 @@ public class VerificationStatus {
                 required = true)
         @NotNull
         private String certificate;
+        @NotNull
         private String status;
         private String reason;
     }
 
+    @Data
+    public static class Hash {
+        private String computed;
+        private String stored;
+    }
 }
 
 
